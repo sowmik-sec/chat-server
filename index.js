b1ecfbea7a701e4870cb985bc6c6e7b8a9afde78
sowmik-sec <sowmik.sec@gmail.com>
2023-05-02 12:30:17 +0600

    backend server initialized

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    const usersCollection = client.db("chatApp").collection("users");
+  } finally {
+  }
+};
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Chat server running");
+});
+
+app.listen(port, () => {
+  console.log(`chat server is running on port ${port}`);
+});
